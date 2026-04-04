7ece3d5fcb1c8cab3f238e22acff0c900da45504
Giacca90 <giacca90mobile@gmail.com>
2026-04-05 01:15:07 +0200

    arreglada transición

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,5 @@
-import {defineCollection, z} from 'astro:content';
+import {defineCollection} from 'astro:content';
+import {z} from 'astro/zod';
 import {parse} from 'csv-parse/sync';
 import fs from 'node:fs';
 import path from 'node:path';
