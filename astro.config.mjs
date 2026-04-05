97c0b2322e7fce33071f7db78e73cdf2c48d32f3
Giacca90 <giacca90mobile@gmail.com>
2026-04-05 14:45:35 +0200

    arreglado modo oscuro y transiciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,27 @@
 import {defineConfig} from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+// https://astro.build/config
 export default defineConfig({
-	// Obligatorio para que los assets (CSS/JS) carguen en GitHub Pages
 	site: 'https://giacca90.github.io',
 	base: '/textil',
+	trailingSlash: 'always',
+
+	// Cambiamos el nombre de la carpeta de salida de '_astro' a 'assets'
+	// para evitar que GitHub Pages/Jekyll la ignoren.
+	build: {
+		assets: 'assets',
+	},
 
 	i18n: {
 		defaultLocale: 'es',
 		locales: ['es', 'ca', 'en', 'fr'],
-		routing: {
-			prefixDefaultLocale: false,
-		},
+		routing: {prefixDefaultLocale: false},
 	},
-
 	vite: {
 		plugins: [tailwindcss()],
-	},
-
-	trailingSlash: 'always',
-	build: {
-		format: 'directory', // Fuerza la estructura de carpetas /index.html
+		build: {
+			cssCodeSplit: false,
+		},
 	},
 });
