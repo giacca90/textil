781758e1c6a464bad05a8c41601e45ee466e68c7
Giacca90 <giacca90mobile@gmail.com>
2026-04-05 12:53:25 +0200

    arregladas rutas para gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,25 @@
 import {defineConfig} from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-	// 1. Configuración de Idiomas
+	// Obligatorio para que los assets (CSS/JS) carguen en GitHub Pages
+	site: 'https://giacca90.github.io',
+	base: '/textil',
+
 	i18n: {
-		defaultLocale: 'es', // El español manda
+		defaultLocale: 'es',
 		locales: ['es', 'ca', 'en', 'fr'],
 		routing: {
-			// 'false' hace que el idioma por defecto (es) NO tenga prefijo /es/
-			// Así la raíz '/' será directamente el contenido de es.csv
 			prefixDefaultLocale: false,
 		},
 	},
 
-	// 2. Configuración de Vite y Tailwind
 	vite: {
 		plugins: [tailwindcss()],
+	},
+
+	trailingSlash: 'always',
+	build: {
+		format: 'directory', // Fuerza la estructura de carpetas /index.html
 	},
 });
